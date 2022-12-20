e13d75906293eb25b5462d861fc21acc39d7445d
Esten Leonardsen <estenleonardsen@gmail.com>
2022-12-20 13:16:13 +0100

    Solved 20

--- 2022/day20/solver.py
+++ 2022/day20/solver.py
@@ -1,17 +1,42 @@
 import argparse
-import re
 import numpy as np
 
-from copy import deepcopy
-from typing import Any, Dict
-from tqdm import tqdm
+from copy import copy
+from typing import List
 
+
+def mix(numbers: List[int], order: List[int]):
+    order = copy(order)
+
+    for i in range(len(order)):
+        idx = order.index(i)
+        value = numbers.pop(idx)
+        destination = (value + idx) % len(numbers)
+        numbers.insert(destination, value)
+        value = order.pop(idx)
+        order.insert(destination, value)
+
+    return numbers, order
 
 def solve(input: str):
     with open(input, 'r') as f:
         numbers = [int(x.strip()) for x in f.readlines()]
 
-    print(numbers)
+    order = np.arange(len(numbers)).tolist()
+
+    single, _ = mix(copy(numbers), copy(order))
+    coords = [single[(single.index(0) + (1000 * i)) % len(single)] \
+             for i in range(1, 4)]
+    print(f'Sum: {np.sum(coords)}')
+
+    numbers = [811589153 * num for num in numbers]
+
+    for _ in range(10):
+        numbers, order = mix(numbers, order)
+
+    coords = [numbers[(numbers.index(0) + (1000 * i)) % len(numbers)] \
+              for i in range(1, 4)]
+    print(f'Sum: {np.sum(coords)}')
 
 
 if __name__ == '__main__':
